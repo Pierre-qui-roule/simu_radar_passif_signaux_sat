63125c07f0ca8e903836f63ad1dc6fcca3356aee
Pierre-qui-roule <pierre13.gonot@gmail.com>
2025-12-19 13:03:37 +0100

    Delete TOUT_LANCER.m

--- TOUT_LANCER.m
+++
@@ -1,9 +0,0 @@
-% Script pour tout lancer de zéro
-disp('1. Calcul Trajectoires...');
-Trajectoire;
-
-disp('2. Calcul Géométrie...');
-calcul_geometrie;
-
-disp('3. Lancement Simulation...');
-main_simulation;
